360d81884ccc22fc9270e90563dd53e8c6569761
unknown <miriam_4@hotmail.co.uk>
2022-06-07 20:10:50 +0100

    initial commit

---
+++ lesson1.py
@@ -0,0 +1,59 @@
+# A1 - Write code that prints ‘Hello world’
+
+print("Hello world")
+
+# A2 - Print the numbers 1 to 5 on a single line
+
+print("1 2 3 4 5")
+
+   
+# A3 - Write a script where ‘Hello’ and ‘World’ are printed on two separate lines
+
+print("Hello\nWorld")
+
+# A4 - Write a script that prints a list of names, tabbed on separate lines, e.g.
+#    ```My List of Names:
+#            Alice
+#            Bob
+#            Charlie
+#    ```
+
+print("My List of Names:\n\tAlice\n\tBob\n\tCharlie")
+
+
+# B1 - Write code that prints the value of 2 + 2
+
+# Do the sum directly within the print statement
+print(2 + 2)
+
+
+# B2 - Write code that prints the value of 5.7 subtracted from 3.4
+
+# Work out the sum and store in a variable before printing out
+sum = 5.7 - 3.4
+print(sum)
+
+   
+# B3 - Write code that prints the value of 8 multiplied by 7
+
+print(8 * 7)
+
+#B4 - Write code that prints the value of 144 divided by 12
+
+# Using brackets to make the sum clear
+result = (144 / 12)
+print(result)
+
+# B5 - Write code that prints the value of the remainder of 67 divided by 12
+
+remainder = 67 % 12
+print(remainder)
+
+# B6 - Write code that finds the value of 20 from `4 - 2 * 6 / 3 * 5`. Hint: you might need brackets
+
+# Without brackets
+print(4 - 2 * 6 / 3 * 5) # 16
+
+# With brackets to ensure the subtraction happens first
+complex_sum = (4 - 2) * 6 / 3 * 5
+print(complex_sum) # 20
